3389896c8699f4f020bd74f7cf3d689025c1a635
VeselAbaya <Vandervise465@gmail.com>
2019-06-25 20:22:28 +0300

    bill page

--- src/app/main/pages/bill/service/bill.service.ts
+++ src/app/main/pages/bill/service/bill.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class BillService {
   constructor(private httpClient: HttpClient) {}
+
+  getBill(): Observable<> {
+
+  }
 }
