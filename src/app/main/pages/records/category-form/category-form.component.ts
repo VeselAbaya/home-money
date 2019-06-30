756e7dbcae3780d0ab4d7c8c9d1c195bc53f712a
VeselAbaya <Vandervise465@gmail.com>
2019-06-30 11:19:22 +0300

    category-form markup

--- src/app/main/pages/records/category-form/category-form.component.ts
+++ src/app/main/pages/records/category-form/category-form.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.scss']
 })
-export class CategoryFormComponent implements OnInit {
+export class CategoryFormComponent {
+  private categoryForm = new FormGroup({
+    name: new FormControl(null, Validators.required),
+    limit: new FormControl(null)
+  });
 
-  constructor() { }
-
-  ngOnInit() {
+  onSubmit() {
+    console.log(this.categoryForm.value);
   }
-
 }
