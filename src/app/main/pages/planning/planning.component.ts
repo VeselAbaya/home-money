0fc75f6490e20bd4dfa49d9002a08efc7ae299d9
VeselAbaya <Vandervise465@gmail.com>
2019-07-14 23:34:18 +0300

    refactoring

--- src/app/main/pages/planning/planning.component.ts
+++ src/app/main/pages/planning/planning.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RecordService } from '../records/service/record.service';
 
 @Component({
   selector: 'app-planning',
   templateUrl: './planning.component.html',
   styleUrls: ['./planning.component.scss']
 })
-export class PlanningComponent {}
+export class PlanningComponent {
+  constructor(private recordService: RecordService) {}
+}
--- src/app/main/pages/records/records.component.ts
+++ src/app/main/pages/records/records.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RecordService } from './service/record.service';
 
 @Component({
   selector: 'app-records',
   templateUrl: './records.component.html',
   styleUrls: ['./records.component.scss']
 })
-export class RecordsComponent {}
+export class RecordsComponent {
+  constructor(private recordService: RecordService) {}
+}
